7b938cd8011169a203ef7a8d4f85dd8229343cac
Lex <Lexperunovsky@gmail.com>
2022-12-21 14:27:48 +0300

    Complete the third Task

---
+++ Task_3/Program.cs
@@ -0,0 +1,60 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+//Вариант решения 1:
+
+/*int DayOfWeek;
+while(true)
+{
+Console.WriteLine("Введите число от 1 до 7: ");
+    if (int.TryParse(Console.ReadLine(), out DayOfWeek))
+        break;
+        Console.WriteLine("Erorr!");
+}
+int weekdays = DayOfWeek;
+int weekend = DayOfWeek;
+if (weekdays <= 5)
+{
+    Console.WriteLine($"{weekdays} - день будний");
+}
+else 
+    Console.WriteLine($"{weekend} - выходной день");*/
+
+
+//Вариант решения 2:
+Console.WriteLine("Введите число от 1 до 7, которое соответсвует дню недели: ");
+int DayOfWeek = int.Parse(Console.ReadLine()??"");
+int day = DayOfWeek;
+if (day == 1) 
+{
+    Console.WriteLine("- понедельник, будний день");
+}
+else if (day == 2)
+{
+    Console.WriteLine("- вторник, будний день");
+}
+else if (day == 3)
+{
+    Console.WriteLine("- среда, будний день");
+}
+else if (day == 4)
+{
+    Console.WriteLine("- четверг, будний день");
+}
+else if (day == 5)
+{
+    Console.WriteLine("- пятница, будний день");
+}
+else if (day == 6)
+{
+    Console.WriteLine("- суббота, выходной день");
+}
+else if (day == 7)
+{
+    Console.WriteLine("- воскресенье, выходной день");
+}
+else 
+Console.WriteLine($"Вы ввели неверное число");
+
+
+
+
+
